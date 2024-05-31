90b76f05c0fc1c863952b228e5a421906b646765
Jing Wang <114956344+wangjing8726@users.noreply.github.com>
2024-05-31 14:32:00 +0200

    Update association_analysis.R

--- protein-metabolite network/association_analysis.R
+++ protein-metabolite network/association_analysis.R
@@ -17,7 +17,6 @@ df_p_m = read_delim("combined_qPCR_proteins_metabolites.txt")
 #### perform linear mixed model regression #######
 print("#### perform linear mixed model regression #######")
 
-
 args=commandArgs(T)
 
 m = print(args)
@@ -55,3 +54,29 @@ df_res = rbind(df_res, res)
 print("############ output data  ###################")
 
 write.table(df_res, file = paste0("results/", m, ".lmm.res.txt"), col.names = T, row.names = F, sep = '\t', quote = F)
+
+###### For each metabolite, the above code was looped and then all the results were combined to calculate the corrected p-values ###############
+
+
+
+### load results
+
+list <- list.files(path="C:/Users/jinwa42/OneDrive - Linköpings universitet/02_project/2022_09_03_mQTL/mQTL_wfr/data/04.m_p_lmm/results",pattern = "lmm.res.txt")
+
+df_lmm_res <- data.frame()
+
+for(i in list){
+  path <- i
+  tmp <- read_delim(file = paste0("C:/Users/jinwa42/OneDrive - Linköpings universitet/02_project/2022_09_03_mQTL/mQTL_wfr/data/04.m_p_lmm/results/",path), delim = "\t", col_names = T) %>% mutate(metabolite = i)
+    
+  df_lmm_res <- rbind(df_lmm_res,tmp)
+}
+
+
+### add p_adj
+df_lmm_res2 = df_lmm_res %>%
+  rename(lmm_p = `p.value`) %>%
+  mutate(lmm_p_adj = p.adjust(lmm_p, method = "fdr")) 
+
+df_lmm_res2 %>%
+  write.table(here::here("output", DOCNAME, "m_p_lmm_res.txt"), col.names = T, row.names = F, sep = "\t", quote = F)
